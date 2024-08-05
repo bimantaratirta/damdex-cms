3d3a0c421b18a5c5e7182538fbaf2623ee55546d
bimantaratirta <bimantarats@gmail.com>
2024-08-05 12:04:48 +0700

    fix: download app module

--- lib/app/modules/newversion.apk/controllers/newversion_apk_controller.dart
+++ lib/app/modules/newversion.apk/controllers/newversion_apk_controller.dart
@@ -1,14 +1,3 @@
-// ignore: avoid_web_libraries_in_flutter
-import 'dart:html' as html;
-
 import 'package:get/get.dart';
 
-class NewversionApkController extends GetxController {
-  @override
-  void onInit() {
-    html.window.open('/assets/assets/apk/damdex-release.apk', 'Download');
-    var newLocation = html.window.location..href = "https://www.damdexindonesia.com/id/";
-    html.window.location = newLocation;
-    super.onInit();
-  }
-}
+class NewversionApkController extends GetxController {}
