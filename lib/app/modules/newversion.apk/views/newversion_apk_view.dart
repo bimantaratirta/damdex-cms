4b3434ddbcb57a6f76f90950100c19c919296e2e
bimantaratirta <bimantarats@gmail.com>
2024-08-08 14:24:22 +0700

    build: release new app version

--- lib/app/modules/newversion.apk/views/newversion_apk_view.dart
+++ lib/app/modules/newversion.apk/views/newversion_apk_view.dart
@@ -46,8 +46,8 @@ class NewversionApkView extends GetView<NewversionApkController> {
                   type: ButtonType.elevated,
                   onPressed: () {
                     html.AnchorElement anchorElement =
-                        html.AnchorElement(href: "/assets/assets/apk/damdex-release-6agst24-1.apk");
-                    anchorElement.download = "damdex-release-6agst24.apk";
+                        html.AnchorElement(href: "/assets/assets/apk/damdex-release-8agst24.apk");
+                    anchorElement.download = "damdex-release-8agst24.apk";
                     anchorElement.click();
                   },
                   child: const Text("Download"),
